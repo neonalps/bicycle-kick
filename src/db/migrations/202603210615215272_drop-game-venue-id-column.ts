084380c9820f0f198d06553bbabb6359b9ca08a2
Michael Stifter <michael@neonalps.at>
2026-03-22 09:52:44 +0100

    chore: drop venue ID column in game table

---
+++ src/db/migrations/202603210615215272_drop-game-venue-id-column.ts
@@ -0,0 +1,9 @@
+import { ColumnDefinitions, MigrationBuilder } from 'node-pg-migrate';
+
+export const shorthands: ColumnDefinitions | undefined = undefined;
+
+export async function up(pgm: MigrationBuilder): Promise<void> {
+    pgm.dropColumn("game", "venue_id");
+}
+
+export async function down(pgm: MigrationBuilder): Promise<void> {}
